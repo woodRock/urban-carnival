2eee0bd28aef306c66fc13160b85e0f7f5e8f993
Jesse Wood <j.r.h.wood98@gmail.com>
2020-08-21 01:16:50 +1200

    Add some basic styles to Repos.

--- src/api/Repos.js
+++ src/api/Repos.js
@@ -2,10 +2,22 @@ import React from "react";
 import { useQuery } from "react-apollo";
 import gql from "graphql-tag";
 
+/**
+ * Github Api GraphiQL explorer available online:
+ * https://developer.github.com/v4/explorer/
+ */
+
 const GET_REPOSITORIES = gql`
-  {
+  query(
+    $limit: Int = 10
+    $field: String = "CREATED_AT"
+    $order: String = "DESC"
+  ) {
     viewer {
-      repositories(first: 10, orderBy: { field: CREATED_AT, direction: DESC }) {
+      repositories(
+        first: $limit
+        orderBy: { field: $field, direction: $order }
+      ) {
         edges {
           node {
             name
@@ -21,11 +33,8 @@ const GET_REPOSITORIES = gql`
 
 const GitRepositories = () => {
   const { loading, error, data } = useQuery(GET_REPOSITORIES);
-
   if (loading) return <h4>Loading</h4>;
-
   if (error) console.log(error);
-
   const {
     viewer: {
       repositories: { edges }
@@ -34,9 +43,11 @@ const GitRepositories = () => {
 
   return (
     <>
-      {edges.map(edge => (
-        <Repository {...edge.node} />
-      ))}
+      <div>
+        {edges.map(edge => (
+          <Repository {...edge.node} />
+        ))}
+      </div>
     </>
   );
 };
@@ -44,10 +55,28 @@ const GitRepositories = () => {
 const Repository = ({ name, description, openGraphImageUrl, url }) => {
   return (
     <>
-      <h1>{name}</h1>
-      <li>{description}</li>
-      <img src={openGraphImageUrl} alt="Repository social preview" />
-      <a href={url}>Link</a>
+      <div
+        style={{
+          width: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+          textAlign: "center",
+          borderStyle: "solid"
+        }}
+      >
+        <h1>{name}</h1>
+        <li>{description}</li>
+        <a href={url}>
+          <img
+            src={openGraphImageUrl}
+            alt="Repository social preview"
+            style={{
+              height: "300px",
+              width: "300px"
+            }}
+          />
+        </a>
+      </div>
     </>
   );
 };
