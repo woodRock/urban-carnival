5721679805f852411f98cbf08a1a593413f2ff40
Jesse Wood <j.r.h.wood98@gmail.com>
2020-08-21 00:23:07 +1200

    Write a proper readme

--- src/api/Apollo.js
+++ src/api/Apollo.js
@@ -1,12 +1,12 @@
-import {ApolloClient} from "apollo-boost";
-import {setContext} from "apollo-link-context";
-import {HttpLink} from "apollo-link-http";
-import {InMemoryCache} from "apollo-boost";
+import { ApolloClient } from "apollo-boost";
+import { setContext } from "apollo-link-context";
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-boost";
 import key from "./key";
 
-const httpLink = new HttpLink({uri: "https://api.github.com/graphql"});
+const httpLink = new HttpLink({ uri: "https://api.github.com/graphql" });
 
-const authLink = setContext((_, {headers}) => {
+const authLink = setContext((_, { headers }) => {
   return {
     headers: {
       ...headers,
